548c2186747f80a0c58a9d1e26b56a2ef90daf31
Andrew Carter <68480855+AndrewCarter04@users.noreply.github.com>
2023-02-25 00:38:45 +0000

    Update game.py
    Wrong file uploaded before

--- game.py
+++ game.py
@@ -1,112 +1,109 @@
+import sys
 import time
 import random
-import sys
 
-def cpuPlay(grid1):
-    amount = 0
-    while True:
-        num = random.choice(grid1)
-        amount += 1
-        if not(num == "x" or num == "o"):
-            break
-        if amount == 1000:
-            num = 101
-            break
-    return int(num) -1
+def printWord(word):
+    newWord = ""
+    for i in range(0, len(word)):
+        newWord += word[i] + " "
+    print(newWord)
 
-def printGrid(grid):
-    print("The current grid:")
-    print("")
-    print(grid[0] + " | " + grid[1] + " | " + grid[2])
-    print(grid[3] + " | " + grid[4] + " | " + grid[5])
-    print(grid[6] + " | " + grid[7] + " | " + grid[8])
-    print("")
-    
+# Hangperson
 
 while True:
+
+    words = ["book","cat","dog","computer"]
+
+    word = random.choice(words)
+    visibleWord = []
+
+    won = ""
+
+    for i in range(0, len(word)):
+        visibleWord.append(i)
+        visibleWord[i] = "*"
+
     print("Welcome!")
     print("")
 
+    print("Here is the word.")
+    print("")
     time.sleep(2)
-
-    print("You will be playing against the computer, and you will be crosses.")
+    printWord(visibleWord)
     print("")
 
-    grid = ["1","2","3","4","5","6","7","8","9"]
-    #availableGrid = ["1","2","3","4","5","6","7","8","9"]
+    #print(visibleWord)
+    #print(word)
 
     time.sleep(2)
 
-    printGrid(grid)
-
-    won = "no"
+    attempts = 0
+    maxAttempts = 10
 
     while True:
-        inputNum = ""
-        while True:
-            inputNum = input("Enter a number from the grid to replace it.")
-            if inputNum.isnumeric():
-                if int(inputNum) < 10 and int(inputNum) > 0:
-                    if not(grid[int(inputNum) - 1] == "x" or grid[int(inputNum) - 1] == "o"):
-                        break
 
-        inputNum = int(inputNum)
+        if maxAttempts == attempts:
+            won = "max attempts"
+            break
+
+        completed = True
+        for i in range(0, len(visibleWord)):
+            if visibleWord[i] == "*":
+                completed = False
+
+        if completed == True:
+            won = "completed"
+            break
                 
-             
 
-        if grid[inputNum -1] == "x" or grid[inputNum - 1] == "o":
-            print("That spot has already been used!")
-            time.sleep(1)
-            inputNum = int(input("Enter another number from the grid to replace it."))
+        print("You have " + str(maxAttempts - attempts) + " attempts remaining.")
+        
+        guessWord = input("Please enter a letter to guess:")
+        guessLetter = guessWord[0].lower()
 
-        grid[inputNum - 1] = "x"
-        #del availableGrid[inputNum - 1]
-        time.sleep(1)
-        printGrid(grid)
+        isLetter = False
 
-        if (grid[0] == grid[1] == grid[2] == "x") or (grid[3] == grid[4] == grid[5] == "x") or (grid[6] == grid[7] == grid[8] == "x") or (grid[0] == grid[3] == grid[6]  == "x") or (grid[1] == grid[4] == grid[7]  == "x") or (grid[2] == grid[5] == grid[8]  == "x") or (grid[0] == grid[4] == grid[8]  == "x") or (grid[2] == grid[4] == grid[6]  == "x"):
-            print("YOU WON!!!")
-            won = "yes"
-            break
+        for i in range(0, len(word)):
+            if word[i] == guessLetter:
+                isLetter = True
+                visibleWord[i] = word[i]
 
-        if cpuPlay(grid) == 100:
-            won = "no winner"
-            break
+        if isLetter:
+            print("That letter is correct! You have gained an extra attempt.")
+        else:
+            print("That letter is not correct!")
+            attempts += 1
 
-        print("It is the computers turn.")
         time.sleep(2)
+
+        print("")
+        printWord(visibleWord)
+        print("")
+
+        time.sleep(2)
+
+    if won == "completed":
+        print("")
+        print("Congrats! You have won!!")
+    elif won == "max attempts":
+        print("")
+        print("You lost! You ran out of attempts.")
+
+    retry = input("Would you like to play again? (Y/N)")
+
+    if retry == "N" or retry == "n":
+        sys.exit()
+    else:
+        print("")
+        
     
-        grid[cpuPlay(grid)] = "o"
-        printGrid(grid)
-
-        if (grid[0] == grid[1] == grid[2] == "o") or (grid[3] == grid[4] == grid[5] == "o") or (grid[6] == grid[7] == grid[8] == "o") or (grid[0] == grid[3] == grid[6]  == "o") or (grid[1] == grid[4] == grid[7]  == "o") or (grid[2] == grid[5] == grid[8]  == "o") or (grid[0] == grid[4] == grid[8]  == "o") or (grid[2] == grid[4] == grid[6]  == "o"):
-            print("YOU LOST!!!")
-            won = "no"
-            break
-
-    if won == "yes":
-        print("")
-        print("You have beaten the computer! Congrats!")
-    elif won == "no":
-        print("")
-        print("The computer has beaten you! Better luck next time.")
-    elif won == "no winner":
-        print("")
-        print("There was no winner, better luck next time!")
-
-    while True:
-        print("")
-        playAgain = input("Do you want to play again? (Y/N)")
-        if playAgain == "y" or playAgain == "Y":
-            print("")
-            break
-        elif playAgain == "n" or playAgain == "N":
-            sys.exit()
+        
             
 
+        
+
+        
+
+        
+
     
-
-
-
-
-
